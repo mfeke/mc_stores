8463bc86063789b1631c8423cd245f353b3d45c4
jnoha501-gif <jnoha501@gmail.com>
2026-08-23 12:24:14 +0000

    update

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -6,9 +6,33 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  email= ""
-  submitEmail(){
+  email?:string
+
+  message:any
+  err: boolean= false
+
+  ngOnInit(){
+
+  }
+  submit(){
+    let vail = isValidEmail(this.email)
+    if(!this.email || vail === false ){
+
+      this.err = true
+
+      return 
+    }
+    this.message = isValidEmail(this.email)
+  }
+  changEmail(){
+   if (this.email){
+    this.err = false 
+    return 
+   }
     
   }
-
 }
+function isValidEmail(email:any) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return emailRegex.test(email);
+      }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,9 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const AUTH_API = 'http://localhost:8080/api/auth/';
-
-const Produt_Api = "http://localhost:8080/kasi/product"
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -16,11 +13,12 @@ const httpOptions = {
 export class AuthService {
 
 
+   jwtb_url = "https://streetfeverapi.vercel.app/"
 
   constructor(private http: HttpClient) { }
   login(body: any): Observable<any> {
-    return this.http.post(AUTH_API ,body,
-      httpOptions);
+    return this.http.post(`${this.jwtb_url}api/auth/signin` ,body,
+   httpOptions);
   }
 
 }
