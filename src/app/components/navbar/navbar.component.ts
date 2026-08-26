7829f6e81c91cd37053d31f823b05deeb0abeb07
booikam-creator <booikam9@gmail.com>
2026-08-26 16:37:13 +0000

    update view products

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,10 +8,20 @@ import { Component, HostListener } from '@angular/core';
 })
 export class NavbarComponent {
   isScrolled = false;
+ cur= '/home'
+  curUrl?:any
+  constructor( private router: Router ) {}
+  
+  ngOnInit(){
+    this.curUrl = this.router.url; 
+    
+  }
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
+    
     // Check if the vertical scroll position is greater than 50px
     this.isScrolled = window.scrollY > 520;
   }
+
 }
