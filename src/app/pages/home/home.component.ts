3f6650611dbae88cdef0004b5a42a5bec4cee1d7
mfekethothulane <111047885+mfeke@users.noreply.github.com>
2026-09-01 10:43:28 +0000

    update

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,5 +7,19 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
-  imageUrl = "https://i.postimg.cc/brZm0j75/1787668656127.jpg"
+  imageUrl = "https://i.postimg.cc/jdMnjtc4/20260901-083903-COLLAGE.jpg"
+  imageUrl2 = "https://i.postimg.cc/V6nXSnT8/1786716684465-2.jpg"
+
+  products = [
+    
+    {
+      image :"https://i.postimg.cc/bwK1Tm1x/1786881238907.jpg",
+      price: 3000 , 
+      name:"7683 Shadow Warrior Advisors"},
+      {
+        image:"https://i.postimg.cc/k5pmph7L/1786706820373.jpg",
+        price:4000,
+        name:'7685 Shadow Warrior Utility Pant'
+      }
+  ]
 }
